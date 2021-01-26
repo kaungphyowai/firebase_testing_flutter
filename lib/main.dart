30c067acc785f8d93dab3f42e6e9c906b7d1010b
NEXTech-MM <developer@nextechmm.com>
2021-01-26 13:35:01 +0630

    background noti complete

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_testing_flutter/screens/wrapper.dart';
 import 'package:firebase_testing_flutter/service/firebase.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,8 +12,54 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   API api = API();
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  String _message = "Genereating Message....";
+
+  String _token = "Genertaing Token.... ";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        setState(() {
+          _message = message.toString();
+        });
+      },
+      // onBackgroundMessage: (Map<String, dynamic> message) async {
+      //   setState(() {
+      //     _message = message.toString();
+      //   });
+      // },
+
+      onLaunch: (Map<String, dynamic> message) async {
+        setState(() {
+          _message = message.toString();
+        });
+      },
+      onResume: (Map<String, dynamic> message) async {
+        setState(() {
+          _message = message.toString();
+        });
+      },
+    );
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      setState(() {
+        _token = token.toString();
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamProvider(
@@ -22,7 +69,15 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         routes: routes,
         themeMode: ThemeMode.dark,
-        home: Wrapper(),
+        home: Scaffold(
+          body: Column(
+            children: [
+              Text(_token),
+              Divider(),
+              Text(_message),
+            ],
+          ),
+        ),
       ),
     );
   }
