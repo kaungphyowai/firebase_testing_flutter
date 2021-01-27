e9ab43968399c1a045a8d16ce5fdeabec7e261c9
NEXTech-MM <developer@nextechmm.com>
2021-01-27 13:19:10 +0630

    8.0.0 dev plugin is working

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:firebase_testing_flutter/service/firebase.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:overlay_support/overlay_support.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,29 +25,55 @@ class _MyAppState extends State<MyApp> {
   String _message = "Genereating Message....";
 
   String _token = "Genertaing Token.... ";
+
   @override
   void initState() {
     super.initState();
+    messaging.setForegroundNotificationPresentationOptions(
+      alert: true, // Required to display a heads up notification
+      badge: true,
+      sound: true,
+    );
+    // AndroidNotificationChannel channel = AndroidNotificationChannel(
+    //   'high_importance_channel', // id
+    //   'High Importance Notifications', // title
+    //   'This channel is used for important notifications.', // description
+    //   importance: Importance.max,
+    // );
+    // final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    //     FlutterLocalNotificationsPlugin();
 
-    var channel = AndroidNotification(channelId: "123");
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Got a message whilst in the foreground!');
-      print('Message data: ${message.data}  ');
-
-      if (message.notification != null) {
-        print('Message also contained a notification: ${message.notification}');
-        showSimpleNotification(
-          Container(child: Text(message.toString())),
-          position: NotificationPosition.top,
-          duration: Duration(
-            minutes: 1,
-          ),
-        );
-        setState(() {
-          _message = message.toString();
-        });
-      }
-    });
+    // flutterLocalNotificationsPlugin
+    //     .resolvePlatformSpecificImplementation<
+    //         AndroidFlutterLocalNotificationsPlugin>()
+    //     ?.createNotificationChannel(channel);
+    // FirebaseMessaging.onBackgroundMessage((message) {
+    //   setState(() {
+    //     _message = message.toString();
+    //   });
+    // });
+    // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    //   RemoteNotification notification = message.notification;
+    //   AndroidNotification android = message.notification?.android;
+    //   print("We are inside onmessage");
+    //   // If `onMessage` is triggered with a notification, construct our own
+    //   // local notification to show to users using the created channel.
+    //   if (notification != null && android != null) {
+    //     flutterLocalNotificationsPlugin.show(
+    //         notification.hashCode,
+    //         notification.title,
+    //         notification.body,
+    //         NotificationDetails(
+    //           android: AndroidNotificationDetails(
+    //             channel.id,
+    //             channel.name,
+    //             channel.description,
+    //             icon: android?.smallIcon,
+    //             // other properties...
+    //           ),
+    //         ));
+    //   }
+    // });
   }
 
   API api = API();
@@ -59,28 +87,29 @@ class _MyAppState extends State<MyApp> {
 
       if (message.notification != null) {
         print('Message also contained a notification: ${message.notification}');
+        setState(() {
+          _message = message.notification.toString();
+        });
       }
     });
     return StreamProvider(
       create: (context) => api.streamingAuthState(context),
-      child: OverlaySupport(
-        child: MaterialApp(
-          title: "News App",
-          debugShowCheckedModeBanner: false,
-          routes: routes,
-          themeMode: ThemeMode.dark,
-          home: Scaffold(
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(_token),
-                Divider(),
-                Text(_message),
-                RaisedButton(onPressed: () {
-                  // showNotification();
-                })
-              ],
-            ),
+      child: MaterialApp(
+        title: "News App",
+        debugShowCheckedModeBanner: false,
+        routes: routes,
+        themeMode: ThemeMode.dark,
+        home: Scaffold(
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(_token),
+              Divider(),
+              Text(_message),
+              RaisedButton(onPressed: () {
+                // showNotification();
+              })
+            ],
           ),
         ),
       ),
